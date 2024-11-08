63612a7a782abebaab3acd09a334da1c12d4287b
sinan <sinanbh9605@gmail.com>
2024-11-08 10:32:51 +0530

    update code in addToCart

--- src/User Pages/component/Cart/CardItems.jsx
+++ src/User Pages/component/Cart/CardItems.jsx
@@ -1,32 +1,24 @@
 import React, { useState, useEffect } from "react";
 import "./Style.css";
 import { useDispatch } from "react-redux";
-import {
-  updateQuantity,
-} from "../../../lib/store/features/cartSlice";
+import { updateQuantity } from "../../../lib/store/features/cartSlice";
 
 function CardItems({ userID, item, onRemove, onQuantityChange }) {
   const dispatch = useDispatch();
-  const [qty, setQty] = useState(item?.quantity || 1);
   const { _id, title, image, price, offerPrice } = item?.prodid || {};
-
-  useEffect(() => {
-    setQty(item.quantity);
-  }, [item?.quantity, _id, userID]);
+  const quantity = item?.quantity || 1; // Get the quantity from the Redux store
 
   const handleAdd = (id) => {
-    const updatedQty = qty + 1;
-    setQty(updatedQty);
+    const newQuantity = quantity + 1;
     dispatch(updateQuantity({ userID, prodid: id, quantityChange: 1 }));
-    onQuantityChange({ [_id]: updatedQty });
+    onQuantityChange({ [_id]: newQuantity }); // Callback to update the parent if needed
   };
 
   const handleRemove = (id) => {
-    if (qty > 1) {
-      const updatedQty = qty - 1;
-      setQty(updatedQty);
+    if (quantity > 1) {
+      const newQuantity = quantity - 1;
       dispatch(updateQuantity({ userID, prodid: id, quantityChange: -1 }));
-      onQuantityChange({ [_id]: updatedQty });
+      onQuantityChange({ [_id]: newQuantity }); // Callback to update the parent if needed
     }
   };
 
@@ -45,11 +37,11 @@ function CardItems({ userID, item, onRemove, onQuantityChange }) {
           <button
             className="btn btn-outline-secondary btn-sm"
             onClick={() => handleRemove(_id)}
-            disabled={qty <= 1}
+            disabled={quantity <= 1}
           >
             -
           </button>
-          <span className="mx-2">{qty}</span>
+          <span className="mx-2">{quantity}</span>
           <button
             className="btn btn-outline-secondary btn-sm"
             onClick={() => handleAdd(_id)}
@@ -59,7 +51,7 @@ function CardItems({ userID, item, onRemove, onQuantityChange }) {
         </div>
         <div className="item-price">
           <h6 className="text-success">
-            Offer $ {(qty * offerPrice).toFixed(2)}
+            Offer $ {(quantity * offerPrice).toFixed(2)}
           </h6>
           <p className="mb-0 text-secondary">
             <del>$ {price}</del>
